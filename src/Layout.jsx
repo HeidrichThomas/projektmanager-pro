30f5fcc4253b310c35c1160b99c065a0b404a869
Thomas H <tommisenden@googlemail.com>
2026-01-08 23:24:53 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -106,6 +106,12 @@ export default function Layout({ children, currentPageName }) {
                                     <NavLink key={item.name} item={item} mobile />
                                 ))}
                             </nav>
+
+                            <nav className="px-4 space-y-1 border-t border-slate-200 pt-4">
+                                {bottomNavigation.map((item) => (
+                                    <NavLink key={item.name} item={item} mobile />
+                                ))}
+                            </nav>
                         </div>
                     </SheetContent>
                 </Sheet>
